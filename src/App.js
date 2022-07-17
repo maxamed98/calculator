ee20a90be9a69110bdfa0af05300db54676da4df
maxamed <98mohamed.mo@gmail.com>
2022-07-17 12:04:29 +0100

    basic structure setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="header"></div>
+        <div className="body"></div>
+      </div>
+      <h5>MD PRODUCTIONS</h5>
     </div>
   );
 }
